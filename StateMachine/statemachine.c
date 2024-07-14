15f6af2690128a24a93536d0c59c582cbf7cfd6f
Brett Dionello <bdionello@uvic.ca>
2024-07-14 14:09:16 -0700

    Added user interface

--- StateMachine/statemachine.c
+++ StateMachine/statemachine.c
@@ -1,9 +1,5 @@
-#include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-# define RAND_MAX 5
 
 typedef enum { VBAT_LOW, VBAT_OK, VBAT_HIGH, VSOL_LOW, VSOL_OK, IBAT_LOW, MAX_EVENT } Event ;
 typedef enum { START, IDLE , CHARGE_M, CHARGE_T, CHARGE_F, MAX_STATE } State ;
@@ -13,6 +9,9 @@ typedef struct {
 Action to_do ; // function pointer to current-state action
 State next_state ; // next-state enumerator
 } Table_Cell ;
+
+const char* event_names[] = { "VBAT_LOW", "VBAT_OK", "VBAT_HIGH", "VSOL_LOW", "VSOL_OK", "IBAT_LOW"};
+const char* state_names[] = { "START", "IDLE" , "CHARGE_M", "CHARGE_T", "CHARGE_F", "MAX_STATE" };
 
 void do_nothing( void );
 void pwm_off( void );
@@ -29,22 +28,24 @@ Table_Cell state_table [ MAX_STATE][ MAX_EVENT ] = {
 { { load_off , START }, { do_nothing , IDLE }, { do_nothing , IDLE }, { do_nothing , IDLE }, { do_nothing , CHARGE_M }, { do_nothing , IDLE } } ,           // IDLE
 { { do_nothing , CHARGE_M }, { load_on , CHARGE_M }, { do_nothing , CHARGE_T }, { pwm_off , IDLE }, { do_nothing , CHARGE_M }, { do_nothing , CHARGE_M} } , // CHARGE_M
 { { do_nothing , CHARGE_T }, { do_nothing , CHARGE_T  }, { do_nothing , CHARGE_T }, { pwm_off , IDLE }, { do_nothing , CHARGE_T }, { do_nothing , CHARGE_F } } ,// CHARGE_T
-{ { do_nothing , CHARGE_F }, { do_nothing , CHARGE_F }, { do_nothing , CHARGE_F }, { pwm_off , IDLE }, { do_nothing , CHARGE_F }, { do_nothing , CHARGE_F } } , // CHARGE_F
+{ { do_nothing , CHARGE_M }, { do_nothing , CHARGE_F }, { do_nothing , CHARGE_F }, { pwm_off , IDLE }, { do_nothing , CHARGE_F }, { do_nothing , CHARGE_F } } , // CHARGE_F
 };
 /* main.c */
 int main(int argc, char *argv[]) {
     Table_Cell state_cell ;
     Event current_event ;
     State current_state ;
-    current_state = START ; // initial state
-    while(1){    
-    current_event = rand();
-    printf("Current Event: %d \n", current_event);
-    printf("Current State: %d \n", current_state);        
-    state_cell = state_table [ current_state ][ current_event ];    
-    state_cell . to_do () ; // execute the appropriate action
-    current_state = state_cell . next_state ; // transition to the new state
-    }    
+    current_state = START ; // initial state    
+    int input;
+    while(1){
+        scanf_s("%d", &input);
+        printf("Current Event: %s \n", event_names[input]);
+        printf("Current State: %s \n", state_names[current_state]);        
+        state_cell = state_table [ current_state ][ input ];    
+        state_cell . to_do () ; // execute the appropriate action
+        current_state = state_cell . next_state ; // transition to the new state
+        printf("Next State: %s \n", state_names[current_state]);
+    };   
 }
 void pwm_off( void ){    
     // Turn off PWM
